19d6ca207fc79e021a5bcc1c54c3187d0f858b1b
Manabu TERADA <terada@cmscom.jp>
2020-08-23 18:52:01 +0900

    modify server app for front

--- src/server/api.py
+++ src/server/api.py
@@ -1,14 +1,17 @@
-from typing import List, Dict
+from typing import List, Dict, Union
+from decimal import Decimal
 from flask import Flask
 from flask import request
 from flask import jsonify
 from flask import render_template
+from flask_cors import CORS
 import boto3
 from boto3.dynamodb.conditions import Key
 
 from config import DYNAMODB_TABLE
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./dist/static", template_folder="./dist")
+cors = CORS(app, resources={r"*": {"origins": ["http://localhost*"]}})
 
 
 def slack_notify(datetime_: str, co2: int) -> bool:
@@ -21,6 +24,13 @@ def save_db(file) -> bool:
     return True
 
 
+def _decimal_to_int(d: Union[int, str, Decimal]):
+    if isinstance(d, str):
+        return d
+    else:
+        return int(d)
+
+
 def fetch_data(date_: str) -> List[Dict[str, int]]:
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(DYNAMODB_TABLE)
@@ -29,18 +39,36 @@ def fetch_data(date_: str) -> List[Dict[str, int]]:
     # response = table.get_item(Key={"date": date_, "time": "1900"})
     # items = response["Item"]
     response = table.query(KeyConditionExpression=Key("date").eq(date_))
-    items = response["Items"]
+    items = [
+        {k: _decimal_to_int(v) for k, v in item.items()} for item in response["Items"]
+    ]
     return items
 
 
 @app.route("/")
-@app.route("/<date_>")
+# @app.route("/<int:date_>")
 def index(date_=None):
+    return render_template("index.html")
+    # app.logger.info(date_)
+    # print(date_)
     if date_ is None:
         items = None
     else:
         items = fetch_data(date_)
     return render_template("index.html", date_=date_, items=items)
+
+
+@app.route("/fetch", methods=["GET", "POST"])
+def fetch():
+    j = request.json
+    date_str = j.get("date")
+
+    if date_str is None:
+        return jsonify([]), 400
+    date_str = date_str.replace("-", "")
+    app.logger.info(date_str)
+    items = fetch_data(date_str)
+    return jsonify(items)
 
 
 @app.route("/save", methods=["POST"])
@@ -77,4 +105,4 @@ def notify():
 
 # We only need this for local development.
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
